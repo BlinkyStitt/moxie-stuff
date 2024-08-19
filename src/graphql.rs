4057056a519ff75a839bffbba3cf4f491f8c5113
Bryan Stitt <bryan@stitthappens.com>
2024-08-18 18:05:08 -0700

    add more query and schema

--- src/graphql.rs
+++ src/graphql.rs
@@ -3,14 +3,15 @@ use graphql_client::GraphQLQuery;
 /// TODO: why doesn't this work? i think the schema is the problem. i think one schema is shared by both queries?
 /// <https://developer.moxie.xyz/use-cases/everyday-rewards/check-users-everyday-rewards-amount>
 pub mod check_claim_transaction_status {
-    // use super::*;
-    //
-    // #[derive(GraphQLQuery)]
-    // #[graphql(
-    //     schema_path = "graphql/check_claim_transaction_status/schema.graphql",
-    //     query_path = "graphql/check_claim_transaction_status/query.graphql"
-    // )]
-    // pub struct FarcasterUserClaimTransactionDetails;
+    use super::*;
+
+    #[derive(GraphQLQuery)]
+    #[graphql(
+        schema_path = "graphql/check_claim_transaction_status/schema.graphql",
+        query_path = "graphql/check_claim_transaction_status/query.graphql",
+        response_derives = "Debug"
+    )]
+    pub struct FarcasterUserClaimTransactionDetails;
 }
 
 /// <https://developer.moxie.xyz/use-cases/everyday-rewards/claim-everyday-rewards>
@@ -27,12 +28,12 @@ pub mod claim_everyday_rewards {
 
 /// TODO: why doesn't this work? i think the schema is the problem. i think one schema is shared by both queries?
 pub mod check_user_everyday_rewards_amount {
-    // use super::*;
-    //
-    // #[derive(GraphQLQuery)]
-    // #[graphql(
-    //     schema_path = "graphql/check_user_everyday_rewards_amount/schema.graphql",
-    //     query_path = "graphql/check_user_everyday_rewards_amount/query.graphql"
-    // )]
-    // pub struct FarcasterUserClaimTransactionDetails;
+    use super::*;
+
+    #[derive(GraphQLQuery)]
+    #[graphql(
+        schema_path = "graphql/check_user_everyday_rewards_amount/schema.graphql",
+        query_path = "graphql/check_user_everyday_rewards_amount/query.graphql"
+    )]
+    pub struct FarcasterUserClaimTransactionDetails;
 }
